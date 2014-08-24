cdc04f87373f40ba8d4c93bada24ce99a0991c9b
kaelzhang <i@kael.me>
2014-08-24 20:19:28 +0800

    first commit

---
+++ test/easing-functions.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var easing_functions = require('../index');
+
+// To know the usage of `assert`, see: http://nodejs.org/api/assert.html
+var assert = require('assert');
+
+describe("description", function(){
+  it("should do blah blah blah", function(){
+    // your code...
+  });
+});
